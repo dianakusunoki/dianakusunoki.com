4cdaa8faa83676e58b0795af0141fe81dd6c3400
Diana Kusunoki <dianakusunoki@users.noreply.github.com>
2019-10-13 18:32:52 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -32,6 +32,23 @@ $(document).ready(function() {
         }
     });
 
-	
+	/* toggle dropdown */
+	function myFunction() {
+	  document.getElementById("myDropdown").classList.toggle("show");
+	}
+
+	/* close the dropdown if the user clicks outside of it */
+	window.onclick = function(event) {
+	  if (!event.target.matches('.dropbtn')) {
+	    var dropdowns = document.getElementsByClassName("dropdown-content");
+	    var i;
+	    for (i = 0; i < dropdowns.length; i++) {
+	      var openDropdown = dropdowns[i];
+	      if (openDropdown.classList.contains('show')) {
+		openDropdown.classList.remove('show');
+	      }
+	    }
+	  }
+	}
 	
 });
