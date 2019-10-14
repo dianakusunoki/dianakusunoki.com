825dd5c50c9fe56eae21db8040ee949ff9e16ed6
Diana Kusunoki <dianakusunoki@users.noreply.github.com>
2019-10-14 03:04:38 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 	
 	$('.dropdown').click(function(e){
 	    $(this).find('.button-container').toggleClass('dropdown-open');
-	    $($(e.target).find('.dropdown-menu').toggleClass('show');
+	    $(this).find.find('.dropdown-menu').toggleClass('show');
 	    $($(e.target).find('.down-caret').toggleClass('open-caret'));
 	    e.preventDefault();
 	    e.stopPropagation();
