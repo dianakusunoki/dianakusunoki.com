6071a62fca306be2d38b367200404d273520168f
Diana Kusunoki <dianakusunoki@users.noreply.github.com>
2019-10-14 01:27:35 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,4 +31,13 @@ $(document).ready(function() {
             }
         }
     });
+
+	$(".dropdown").on("hide.bs.dropdown", function(){
+		$(".btn").html('Dropdown <span class="caret"></span>');
+	});
+	
+	$(".dropdown").on("show.bs.dropdown", function(){
+		$(".btn").html('Dropdown <span class="caret caret-up"></span>');
+	});
+	
 });
