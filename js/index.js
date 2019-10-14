da0a85b91c59b89ce20c781d9cc0493f6275e702
Diana Kusunoki <dianakusunoki@users.noreply.github.com>
2019-10-14 02:22:58 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,12 +33,12 @@ $(document).ready(function() {
     });
 	
 	$('.dropdown').click(function(e){
-	    $(this).find('.dropdown-menu').toggleClass('show');
+	    $(this).find('.dropdown-menu').toggleClass('open');
 	    $($(e.target).find('.down-caret').toggleClass('open-caret');
 	    e.preventDefault();
 	    e.stopPropagation();
 	    $(document).click(function(){
-	      $('.dropdown-menu').removeClass('show');
+	      $('.dropdown-menu').removeClass('open');
 	      $('.down-caret').removeClass('open-caret');
 	    });
 	  });
